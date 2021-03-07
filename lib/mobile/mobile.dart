ffd151c1aca7e5dd8fd07bcf719d44d847620d69
venkat <venki14101996@gmail.com>
2021-03-07 17:33:30 +0530

    grid view created

--- lib/mobile/mobile.dart
+++ lib/mobile/mobile.dart
@@ -1,35 +1,75 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 
 class MobileView extends StatelessWidget {
+  const MobileView({Key key}) : super(key: key);
+
+  //* BUILD METHOD FOR CUSTOMSCROLLVIEW WITH SLIVERS (SLIVERGRID)
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: CustomScrollView(
-        slivers: <Widget>[
-          SliverAppBar(
-            expandedHeight: 200.0,
-            flexibleSpace: const FlexibleSpaceBar(
-              title: Text('Available seats'),
-            ),
-            actions: <Widget>[
-              IconButton(
-                icon: const Icon(Icons.add_circle),
-                tooltip: 'Add new entry',
-                onPressed: () {},
+    return FutureBuilder(
+      future: _loadImagePaths(context),
+      builder: (
+        BuildContext context,
+        AsyncSnapshot<List<String>> imagePathsSnapshot,
+      ) {
+        if (imagePathsSnapshot.connectionState == ConnectionState.done &&
+            imagePathsSnapshot.hasData) {
+          return CustomScrollView(
+            slivers: <Widget>[
+              SliverPadding(
+                padding: const EdgeInsets.all(10),
+                sliver: _buildContent(imagePathsSnapshot.data),
               ),
             ],
-          ),
-          SliverList(
-            delegate: SliverChildListDelegate([
-              Text("1"),
-              Text("1"),
-              Text("1"),
-              Text("1"),
-            ]),
-          ),
-        ],
+          );
+        }
+
+        return Center(child: CircularProgressIndicator());
+      },
+    );
+  }
+
+  //* SLIVERGRID
+
+  SliverGrid _buildContent(List<String> imagePaths) {
+    return SliverGrid(
+      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+        maxCrossAxisExtent: 150,
+        mainAxisSpacing: 10,
+        crossAxisSpacing: 10,
       ),
-      drawer: Drawer(),
+      delegate: SliverChildBuilderDelegate(
+        (BuildContext context, int index) {
+          return _buildImageWidget(imagePaths[index]);
+        },
+        childCount: imagePaths.length,
+      ),
+    );
+  }
+
+  Future<List<String>> _loadImagePaths(BuildContext context) async {
+    final String manifestContentJson =
+        await DefaultAssetBundle.of(context).loadString('AssetManifest.json');
+
+    final Map<String, dynamic> manifestMap = json.decode(manifestContentJson);
+
+    return manifestMap.keys
+        .where((String key) => key.contains('assets/images/'))
+        .toList();
+  }
+
+  Widget _buildImageWidget(String imagePath) {
+    return Container(
+      child: ClipRRect(
+        borderRadius: BorderRadius.all(Radius.circular(10)),
+        child: Image.asset(
+          imagePath,
+          fit: BoxFit.cover,
+        ),
+      ),
     );
   }
 }
